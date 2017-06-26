7d06dfb6970579214dd8dab63dc1801b468b8c75
Yen <yenchieh86@hotmail.com>
2017-06-26 04:33:12 +0000

    Add create new item feature

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,3 +1,19 @@
-$('li').click(function() {
+$('ul').on('click', 'li', function() {
     $(this).toggleClass('completed');
 });
+
+$('ul').on('click', 'span', function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text'").keypress(function(event) {
+    if(event.which === 13) {
+        var userInput = $(this).val()
+        $('ul').append('<li><span>delete</span> ' + userInput + '</li>');
+        $(this).val('')
+    }
+    
+})
